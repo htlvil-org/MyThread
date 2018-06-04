dbdcc15c3d5e8cee7d477718d9b5e62ca2cc300f
org <schueler@ubuntu>
2018-06-04 12:42:27 +0200

    1st step

---
+++ DeleteMe/src/pkgData/Car.java
@@ -0,0 +1,15 @@
+package pkgData;
+
+import java.io.Serializable;
+
+public class Car implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	int id;
+	String name;
+	
+}
